0943db5e1e0450d5e7d5cdb62eb214a8c4d2a95a
Nekliukov <roman.nekliukov@gmail.com>
2018-08-26 21:29:52 +0300

    Paths in db realisation, migrated from NET.S.2018.NEKLIUKOV repository

---
+++ PicturesGallery/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PicturesGallery.Models;
+
+namespace PicturesGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private PicturesStoreEntities db = new PicturesStoreEntities();
+
+        // GET: Home
+        public ActionResult Index() => View(db.PicturesTable.ToList());
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
